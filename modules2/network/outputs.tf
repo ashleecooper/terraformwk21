fc3e22f33bbf1bb19f8f90294166b351964a02f8
Ashlee Cooper <ashleedolan1@yahoo.com>
2022-10-31 10:47:38 -0500

    Create outputs.tf

---
+++ modules2/network/outputs.tf
@@ -0,0 +1,34 @@
+# --- network/outputs.tf
+
+
+output "pubsg_name" {
+  value = aws_security_group.public_SG.name
+}
+
+output "privsubid" {
+  value = aws_subnet.privsub[*].id
+}
+
+output "vpc_id" {
+  value = aws_vpc.projectVPC.id
+}
+
+output "listedAZs" {
+  value = var.azs
+}
+
+output "privSG_id" {
+  value = aws_security_group.private_SG.id
+}
+
+output "pubSGid" {
+  value = aws_security_group.public_SG.id
+}
+
+output "publicsubid" {
+  value = aws_subnet.pubsub[0].id
+}
+
+output "privSGname" {
+  value = aws_security_group.private_SG.name
+}
