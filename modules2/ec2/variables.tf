c2e06e9ec97a20d6c9b3129de0ae57ddfaafb348
Ashlee Cooper <ashleedolan1@yahoo.com>
2022-10-31 10:45:29 -0500

    Create variables.tf

---
+++ modules2/ec2/variables.tf
@@ -0,0 +1,17 @@
+# --- ec2/variables.tf
+
+variable "instance_type" {}
+variable "tags" {}
+variable "pubsg_name" {}
+variable "key_name" {}
+variable "listedAZs" {}
+variable "sg_id" {}
+variable "tag" {}
+variable "targetARN" {}
+variable "ami_id" {}
+variable "privsubids" {}
+variable "albarn" {}
+variable "pubSGid" {}
+variable "publicsubid" {}
+variable "privSGname" {}
+variable "privSGid" {}
